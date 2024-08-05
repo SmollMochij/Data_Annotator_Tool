8b55957bf6cb35591fb0dd9d739693d6b76ca9b4
Nathan Sellar <nathansellar15@gmail.com>
2024-08-06 02:01:43 +1200

    wip: create dashboard page
    dashboard "layout" page and "new" page are being worked on. There are additional html files for testing things like chart.js features

---
+++ scripts/dashboard.js
@@ -0,0 +1,17 @@
+// function printDivSize(div) {
+//     console.log(`${div}\n-----------------------\n`);
+//     console.log(`Client width: ${div.clientWidth}\nScroll width: ${div.scrollWidth}\nOffset width: ${div.offsetWidth}`)
+//     console.log(`Client height: ${div.clientHeight}\nScroll height: ${div.scrollHeight}\nOffset height: ${div.offsetHeight}`)
+// }
+
+// window.onload = function() {
+//     printDivSize(document.getElementById("card1"));
+//     printDivSize(document.getElementById("myChart"));
+// }
+
+// window.onresize = function() {
+//     let card = document.getElementById("card1body");
+//     let donutChart = document.getElementById("myChart");
+//     printDivSize(card);
+//     printDivSize(donutChart)
+// }
